1012573122938f2467178410534c712c12c417fd
KhomenkoVladyslav <khomenko.vlad@ukr.net>
2023-07-09 16:54:26 +0300

    fix
    "greeting" before main menu
    python3 --> python
    remove input("Press enter to continue.")
    exit --> 4
    test.py --> bot.py

--- main.py
+++ main.py
@@ -13,37 +13,34 @@ def print_menu():
     print("║          3. NOTE BOOK MENU                     ║")
     print("╠────────────────────────────────────────────────╣")
     print("║                                                ║")
-    print("║                   EXIT                         ║")
+    print("║          4.        EXIT                        ║")
     print("║                                                ║")
     print("╚════════════════════════════════════════════════╝")
 
 def run_address_book():
-    os.system("python3 AdressBook/test.py")
+    os.system("python AdressBook/bot.py")
 
 def run_folder_sorter():
-    os.system("python3 FolderSorter/test.py")
+    os.system("python FolderSorter/bot.py")
 
 def run_note_book():
-    os.system("python3 NoteBook/test.py") 
+    os.system("python NoteBook/bot.py")
 
 def main_menu():
     while True:
         print_menu()
-        choice = input("You are in \"MAIN MENU\". Choose one of the options: \n>>> ")
+        choice = input("\n>>> ")
 
         if choice == "1":
             run_address_book()
-            input("Press enter to continue.")
 
         elif choice == "2":
             run_folder_sorter()
-            input("Press enter to continue.")
 
         elif choice == "3":
             run_note_book()
-            input("Press enter to continue.")
 
-        elif choice.lower() == "exit":
+        elif choice.lower() == "4":
             print("Exit")
             break
 
