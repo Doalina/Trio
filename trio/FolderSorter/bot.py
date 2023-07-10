a642313141c39e307ca4a17a10ff05e5dceb49c3
ani <125677472+anilev6@users.noreply.github.com>
2023-07-10 22:39:30 +0200

    upd

--- trio/FolderSorter/bot.py
+++ trio/FolderSorter/bot.py
@@ -231,4 +231,6 @@ def launch():
             print("something went wrong! please check your path input")
 # else:
 #     print("Please enter 'clean-folder *a directory to sort here*' ")
-# launch()
+if __name__ == '__main__':
+    launch()
+    
